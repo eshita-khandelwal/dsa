0302f6ef5921cca2c42423b210d4598555bbf1e9
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2025-06-30 08:58:09 -0700

    Time: 56 ms (79.36%), Space: 20.8 MB (43.17%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,32 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        
+        nei = collections.defaultdict(list) #adjaceny list
+        wordList.append(beginWord)
+
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j+1:]
+                nei[pattern].append(word)
+        
+        #traversal code using BFS
+        res = 1
+        q = deque([beginWord])
+        visit = set([beginWord])
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j+1:]
+                    for neiWord in nei[pattern]:
+                        if neiWord not in visit:
+                            visit.add(neiWord)
+                            q.append(neiWord)
+            res+=1
+        return 0
+
+
