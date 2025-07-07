844d07c8f40fa874e6fcedfd24437f79becff5fa
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2025-07-06 20:07:49 -0700

    Time: 4690 ms (35.27%), Space: 18 MB (20.95%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,22 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        #time complexity is O( n * m *dfs) => O(n*m * 4^p) p is the length of the word
+        rows = len(board)
+        cols = len(board[0])
+        visit = set()
+        def dfs(r,c,i):
+            if i==len(word):
+                return True
+            if r<0 or c<0 or r>=rows or c>=cols or (r,c) in visit or board[r][c]!=word[i]:
+                return False
+            
+            visit.add((r,c))
+            res = dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or dfs(r,c-1,i+1)
+            visit.remove((r,c))
+            return res
+        
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r,c,0):
+                    return True
+        return False
