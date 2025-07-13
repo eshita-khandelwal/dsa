b1dbfbd4bf56499ab4a0f806c2e9018f95c56ec2
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2025-07-13 11:24:23 -0700

    Time: 3 ms (29.63%), Space: 18.1 MB (25.51%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,19 +1,15 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        #open < n , closed<open, open==closed==n : add to the result
-        result = []
-        stack = [] #used to add and pop, it is a global variable
-        def backtrack(openN,closedN):
-            if closedN == openN==n:
-                result.append("".join(stack))
+        def generate(o,c,s,n):
+            nonlocal res
+            if o == c == n:
+                res.add(s)
                 return
-            if openN<n:
-                stack.append("(")
-                backtrack(openN+1,closedN)
-                stack.pop()
-            if closedN<openN:
-                stack.append(")")
-                backtrack(openN,closedN+1)
-                stack.pop()
-        backtrack(0,0)
-        return result
+            if o<=n:
+                generate(o+1,c,s+'(',n)
+            if c<o:
+                generate(o,c+1,s+')',n)
+
+        res = set()
+        generate(0,0,'',n)
+        return list(res)
