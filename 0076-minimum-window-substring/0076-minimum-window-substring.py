64e263d78b0908936b4c732d824877a1f6a36ddf
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2025-07-20 22:37:17 -0500

    Time: 72 ms (59.96%), Space: 18.3 MB (34.86%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,33 +1,31 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) == 0 or len(t) ==0:
-            return ""
-        countT, window = {},{} #hashmap
-
-        for c in t:
-            countT[c] = 1 + countT.get(c,0)
+        count = {}
+        for i in t:
+            count[i] = 1 + count.get(i,0)
         
-        l = 0
-        res = [-1,-1]
-        resl = float("infinity")
-        have = 0 
-        need = len(countT)
-
-        for r in range(len(s)):
-            window[s[r]] = 1 + window.get(s[r],0)
-            if s[r] in countT and countT[s[r]] == window[s[r]]:
+        countS = {}
+        r=l=0
+        res = float("infinity")
+        ans = [-1,-1]
+        have = 0
+        need = len(count)
+        while r<len(s):
+            countS[s[r]] = 1 + countS.get(s[r],0)
+            if s[r] in count and count[s[r]] == countS[s[r]]:
                 have+=1
-
             while have == need:
-                if r-l+1 < resl:
-                    resl=r-l+1
-                    res = [l,r]
-                window[s[l]] -=1
-                
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
-                    have-=1
+                if r-l+1<res:
+                    res = r-l+1
+                    ans[0] = l
+                    ans[1] = r
+                countS[s[l]]-=1
+
+                if s[l] in count and countS[s[l]]<count[s[l]]:
+                    have -=1
                 l+=1
-        l,r = res
-        return s[l:r+1] if resl<float("infinity") else ""
+            r+=1
+        return s[ans[0]:ans[1]+1]
 
 
+                
