f96586c4f0d62472a1b4576156505ac7a81aff03
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2025-07-30 19:16:14 -0500

    Time: 747 ms (63.01%), Space: 18.4 MB (44.18%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,10 +1,10 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [(amount+1)] * (amount+1)
+        #use bottom up approch and time complexity is o(amount * len(coins))
+        dp = [amount+1] * (amount+1)
         dp[0] = 0
         for a in range(1,amount+1):
             for c in coins:
                 if a-c>=0:
-                    dp[a] = min( dp[a], 1 + dp[a-c])
-        
+                    dp[a] = min(dp[a],dp[a-c]+1)
         return dp[amount] if dp[amount]!=amount+1 else -1
