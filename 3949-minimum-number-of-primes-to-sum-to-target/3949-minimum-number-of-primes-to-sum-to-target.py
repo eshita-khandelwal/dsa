69c2360cd72e1c8bf5952668f35999fb06a4939c
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2025-07-30 19:23:54 -0500

    Time: 819 ms (57.85%), Space: 17.7 MB (93.57%) - LeetHub

--- 3949-minimum-number-of-primes-to-sum-to-target/3949-minimum-number-of-primes-to-sum-to-target.py
+++ 3949-minimum-number-of-primes-to-sum-to-target/3949-minimum-number-of-primes-to-sum-to-target.py
@@ -1,27 +1,23 @@
 class Solution:
     def minNumberOfPrimes(self, n: int, m: int) -> int:
-        dp = [n+1] * (n+1)
-        dp[0] = 0
+        #same as coin change dp problem lets build the prime array first
         k = m
-        #get all the primes 
         coins = []
-        def is_prime(x):
-            if x < 2:
-                return False
-            for i in range(2, int(x ** 0.5) + 1):
-                if x % i == 0:
+        def isPrime(i):
+            for j in range(2,i):
+                if i%j == 0:
                     return False
             return True
-        
-        for c in range(2,n+1):
-            if k==0:
+        for i in range(2,n+1):
+            if isPrime(i):
+                coins.append(i)
+            if len(coins) == m:
                 break
-            if is_prime(c):
-                coins.append(c)
-                k -=1
-       
+        dp = [n+1] * (n+1)
+        dp[0] = 0
+        
         for a in range(1,n+1):
             for c in coins:
-                if a-c >=0:
+                if a-c>=0:
                     dp[a] = min(dp[a],1+dp[a-c])
         return dp[n] if dp[n]!=n+1 else -1
