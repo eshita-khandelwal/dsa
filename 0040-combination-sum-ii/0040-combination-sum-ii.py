51fef19a5c33539fb46629e62989f1af5ac050b9
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2025-08-15 22:19:27 -0500

    Time: 31 ms (24.1%), Space: 18 MB (35.69%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,20 +1,21 @@
 class Solution:
-    def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        nums.sort()
-        def dfs(i,sum1,cur):
-            nonlocal res
-            if sum1==target:
-                res.append(cur.copy())
+        candidates.sort()
+        def dfs(i,sum1,subset):
+            if sum1 == target:
+                res.append(subset.copy())
                 return
-            for idx in range(i,len(nums)):
-                if sum1 + nums[idx] > target:
-                    break
-                if idx > i and nums[idx] == nums[idx-1]:
-                    continue
-                cur.append(nums[idx])
-                dfs(idx+1,sum1+nums[idx],cur)
-                cur.pop()
-        
+            if i == len(candidates) or sum1 > target:
+                return
+            sum1 +=candidates[i]
+            subset.append(candidates[i])
+            dfs(i+1,sum1,subset)
+            sum1 -=candidates[i]
+            subset.pop()
+            j = i + 1
+            while j < len(candidates) and candidates[j] == candidates[i]:
+                j += 1
+            dfs(j, sum1, subset)
         dfs(0,0,[])
         return res
