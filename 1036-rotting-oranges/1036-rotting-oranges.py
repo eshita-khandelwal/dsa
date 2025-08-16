c8b7a5d3a53c8118b05d116bceffe0d59e3132e3
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2025-08-16 17:35:06 -0500

    Time: 1 ms (89.7%), Space: 17.7 MB (71.62%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,36 +1,41 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        n,m = len(grid),len(grid[0])
         q = deque()
-        time,fresh = 0,0
-        row = len(grid)
-        col = len(grid[0])
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] ==2:
-                    q.append([i,j])
-                if grid[i][j] == 1:
-                    fresh +=1
-        
-        while q and fresh > 0:
+        seen = set()
+        fcount = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 2:
+                    q.append((i,j))
+                elif grid[i][j] == 1:
+                    fcount +=1
+
+        minutes = 0
+        while q:
             for i in range(len(q)):
                 r,c = q.popleft()
-                if r>=0 and c+1<col and grid[r][c+1] == 1:
-                    q.append([r,c+1])
-                    grid[r][c+1] = 2
-                    fresh -=1
-                if r>=0 and c-1>=0 and grid[r][c-1] == 1:
-                    q.append([r,c-1])
-                    grid[r][c-1] = 2
-                    fresh -=1
-                if r+1<row and c<col and grid[r+1][c] == 1:
-                    q.append([r+1,c])
+                #grid[r][c] = 0
+                if r+1 < n and grid[r+1][c]==1:
+                    q.append((r+1,c))
                     grid[r+1][c] = 2
-                    fresh -=1
-                if r-1>=0 and c<col and grid[r-1][c] == 1:
-                    q.append([r-1,c])
+                    fcount -=1
+                if r-1>=0 and grid[r-1][c]==1:
+                    q.append((r-1,c))
                     grid[r-1][c] = 2
-                    fresh -=1
-            time+=1
-        
-        return time if fresh == 0 else -1
-            
+                    fcount -=1
+                if c+1 < m and grid[r][c+1]==1:
+                    q.append((r,c+1))
+                    grid[r][c+1] = 2
+                    fcount -=1
+                if c-1>=0 and grid[r][c-1]==1:
+                    q.append((r,c-1))
+                    grid[r][c-1] = 2
+                    fcount -=1
+            if q:
+                minutes +=1
+        return minutes if fcount == 0 else -1
+                
+
+
+
