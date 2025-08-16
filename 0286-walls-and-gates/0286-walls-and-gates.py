9af3dfac7acb45968b36cf264eb6745774cc3086
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2025-08-16 17:05:01 -0500

    Time: 40 ms (59.4%), Space: 25.1 MB (20.3%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -3,31 +3,36 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        #time complexity is O(mn)
-        n,m = len(rooms), len(rooms[0])
+        n,m = len(rooms),len(rooms[0])
         q = deque()
-        visit = set()
-        def addToQ(r,c):
-            if r<0 or r==n or c<0 or c==m or (r,c) in visit or rooms[r][c] == -1:
-                return
-            q.append([r,c])
-            #visit.add((r,c))
-
+        seen = set()
         for i in range(n):
             for j in range(m):
-                if rooms[i][j] == 0:
-                    q.append([i,j])
-                    #visit.add((i,j))
-        dist = 0
+                if rooms[i][j]==0:
+                    q.append((i,j,0))
+                    seen.add((i,j))
+        
+        
         while q:
             for i in range(len(q)):
-                r,c = q.popleft()
+                r,c,dist = q.popleft()
                 rooms[r][c] = min(dist,rooms[r][c])
-                visit.add((r,c))
-                addToQ(r+1,c)
-                addToQ(r-1,c)
-                addToQ(r,c-1)
-                addToQ(r,c+1)
-            dist+=1
+                if r+1< n and rooms[r+1][c]!=-1 and (r+1,c) not in seen:
+                    q.append((r+1,c,dist+1))
+                    seen.add((r+1,c))
+                    # rooms[r+1][c] = dist+1
+                if c + 1< m and rooms[r][c+1]!=-1  and (r,c+1) not in seen:
+                    q.append((r,c+1,dist+1))
+                    seen.add((r,c+1))
+                    # rooms[r][c+1] = dist+1
+                if r-1>= 0 and rooms[r-1][c]!=-1  and (r-1,c) not in seen:
+                    q.append((r-1,c,dist+1))
+                    rooms[r-1][c] = dist+1
+                    seen.add((r-1,c))
+                if c-1>= 0 and rooms[r][c-1]!=-1  and (r,c-1) not in seen:
+                    q.append((r,c-1,dist+1))
+                    seen.add((r,c-1))
+                    # rooms[r][c-1] = dist+1
         
 
+                
