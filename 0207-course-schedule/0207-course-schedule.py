b83420851aea2eb94be8d1b5bbe73f9067975fe9
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2025-08-17 14:33:36 -0500

    Time: 8 ms (24.1%), Space: 19 MB (83.33%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,24 +1,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #indegree is important here ? - finds cycles
-        q = deque()
-        graph = {i:[] for i in range(numCourses)}
+        adj = {i:[] for i in range(numCourses)} 
         indegree = [0] * numCourses
-
-        for i in range(len(prerequisites)):
-            graph[prerequisites[i][1]].append(prerequisites[i][0])
-            indegree[prerequisites[i][0]] +=1
+        q = deque()
+        for a,b in prerequisites:
+            adj[b].append(a)
+            indegree[a] +=1
         for i in range(numCourses):
-            if indegree[i] == 0:
+            if indegree[i]==0:
                 q.append(i)
         nodesVisited = 0
-
         while q:
-            cur = q.popleft()
+            course = q.popleft()
             nodesVisited +=1
-            for nei in graph[cur]:
-                indegree[nei] -=1
-                if indegree[nei] == 0:
-                    q.append(nei)
+            for neiCourse in adj[course]:
+                indegree[neiCourse]-=1
+                if indegree[neiCourse] == 0:
+                    q.append(neiCourse)
         
         return nodesVisited == numCourses
+
+
+        
