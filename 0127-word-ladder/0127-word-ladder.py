f5666b5a6c6a61ea56ae4ca0b8af41d4de514e70
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2025-08-17 21:46:39 -0500

    Time: 501 ms (19.55%), Space: 21.2 MB (29.37%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,31 +2,29 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
-        
-        nei = collections.defaultdict(list) #adjaceny list
         wordList.append(beginWord)
-
-        for word in wordList:
-            for j in range(len(word)):
-                pattern = word[:j] + "*" + word[j+1:]
-                nei[pattern].append(word)
-        
-        #traversal code using BFS
+        adj = collections.defaultdict(list)
+        for w in wordList:
+            for j in range(len(w)):
+                pattern = w[:j] + '*' + w[j+1:]
+                adj[pattern].append(w)
+        q = deque()
+        q.append(beginWord)
+        visit = set()
         res = 1
-        q = deque([beginWord])
-        visit = set([beginWord])
         while q:
             for i in range(len(q)):
                 word = q.popleft()
+                visit.add(word)
                 if word == endWord:
                     return res
                 for j in range(len(word)):
-                    pattern = word[:j] + "*" + word[j+1:]
-                    for neiWord in nei[pattern]:
-                        if neiWord not in visit:
-                            visit.add(neiWord)
-                            q.append(neiWord)
-            res+=1
+                    pattern = word[:j] + '*' + word[j+1:]
+                    for nei in adj[pattern]:
+                        if nei not in visit:
+                            q.append(nei)
+            res +=1  
         return 0
+            
 
 
