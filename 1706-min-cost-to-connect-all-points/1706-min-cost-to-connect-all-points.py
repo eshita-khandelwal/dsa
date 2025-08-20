d2cc25a4e508e7bea707c3796a10dc2a9dafa312
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2025-08-19 21:41:48 -0500

    Time: 5405 ms (5.35%), Space: 176.1 MB (19.71%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -1,23 +1,28 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        #we will use minimum spanning trees algorithm and we will use prim's algo for that
         adj = collections.defaultdict(list)
         for i in range(len(points)):
             for j in range(i+1,len(points)):
-                dist = abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1])
+                dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
                 adj[i].append([dist,j])
                 adj[j].append([dist,i])
+        
         minHeap = []
         heapq.heappush(minHeap,[0,0])
         visit = set()
         res = 0
-        while len(visit)<len(points):
-            cost,v = heapq.heappop(minHeap)
-            if v in visit:
+        while minHeap:
+            cost,node = heapq.heappop(minHeap)
+            if node in visit:
                 continue
+            visit.add(node)
             res +=cost
-            visit.add(v)
-            for neiC, nei in adj[v]:
+            for neiC,nei in adj[node]:
                 if nei not in visit:
                     heapq.heappush(minHeap,[neiC,nei])
+        
         return res
+
+
+
+            
